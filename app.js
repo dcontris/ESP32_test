fe1365fbe7b76f19020eb28d1515cb3b87e1d0d0
Dylan <ArtTatum19@hotmail.com>
2017-09-19 21:10:49 -0700

    Expanded plotly streaming to multivariate plot

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-require("!style-loader!css-loader!./client/style.css");
-var config = require('./client/plotly_config.json');
-var username = config['username'];
-var apiKey = config.apiKey;
-var token = config.streamingToken;
-
 function httpGet(url) {
     x = new XMLHttpRequest();
     x.open('GET', url, false)
@@ -13,26 +7,47 @@ function httpGet(url) {
 };
 
 function read_sensor() { 
-    var reading = httpGet('http://192.168.1.12:8080/');
-    return reading;
+    var reading = httpGet('http://192.168.1.6:8080/');
+
+    return JSON.parse(reading);
   }
   
-  Plotly.plot('test', [{
+  Plotly.plot('test', [
+    {
     y: [],
     mode: 'lines',
-    line: {color: '#80CAF6'}
-  }]);
+    line: {color: '#80CAF6'},
+    name:'X'
+  },
+  {
+    y: [],
+    mode: 'lines',
+    line: {color: '#000000'},
+    name:'Y'
+  },
+  {
+    y: [],
+    mode: 'lines',
+    line: {color: '#cad400'},
+    name:'Z'
+  },
+]);
   
   var cnt = 0;
-  
+  var reading = 0;
+
   var interval = setInterval(function() {
-    
-    Plotly.extendTraces('test', {
-      y: [[read_sensor()]]
-    }, [0])
+    reading = read_sensor();
+    reading = [reading.x, reading.y, reading.z];
+
+    Plotly.extendTraces('test', 
+      {
+        y: reading
+      }
+    , [0,1,2])
     cnt++
   
     if(cnt === 1000) clearInterval(interval);
-  }, 300);
+  }, 1000);
 
   
